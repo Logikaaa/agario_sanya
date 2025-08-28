42705e1d887b47e42d7f7019f24293d1a70829e0
Logikaaa <logika.kostopil@gmail.com>
2025-08-28 15:24:10 +0300

    Оновленя сервера

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from socket import*
 import time
+from threading import Thread
 
 sock = socket(AF_INET, SOCK_STREAM)
 sock.bind("localhost", 2512)
 
 sock.listen(5) 
+sock.setblocking(False)
 
 players = {}
 id_counter = 0
@@ -16,4 +18,15 @@ def handle_data():
         player_data = {}
         to_remove = []
 
-        
+        for conn in list(players):
+            try:
+                data = conn.recv(1024).decode().stript()
+                if ',' in data:
+                    parts = data.split(',')
+                    if len(parts) == 4:
+                        pid, x, y, r  = map(int, parts)
+                        players[conn] = {'id': pid, 'x': x, 'y': y, 'r': r,}
+                        player_data[conn] = players[conn]
+            except:
+                continue
+Thread(target=handle_data, daemon=True).start()
